425bf65917faf9452ec03381cc66ee863441bf7f
tasthana <tasthana@outlook.com>
2023-06-21 15:23:30 -0400

    changes to ajax footer and footer links

--- final/footer.php
+++ final/footer.php
@@ -1,7 +1,7 @@
 
 <footer>
     <p><a href="../sitemap.php">Site Map</a></p>
-    <p><a href="https://tasthana.w3.uvm.edu/cs145/sitemap.php">Final Project</a></p>
+    <p><a href="https://tasthana.w3.uvm.edu/cs145/final/index.php">Final Project</a></p>
     
 </footer>
     
