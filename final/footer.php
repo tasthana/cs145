8aafe0feec5110860fab664601169594eef879c1
tasthana <tasthana@outlook.com>
2023-06-29 19:24:12 -0400

    added my name to footer, according to project reqs

--- final/footer.php
+++ final/footer.php
@@ -4,6 +4,7 @@
 <footer>
     <p><a href="../sitemap.php">Site Map</a></p>
     <p><a href="https://tasthana.w3.uvm.edu/cs145/final/index.php">Final Project</a></p>
+    <p>Site made by Tushar Asthana</p>
     
 </footer>
     
